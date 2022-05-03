3a7891550226f560d5e2fdf7b86e95d4dfda2709
Jens Schneider <schneider@23technologies.cloud>
2022-05-03 10:47:35 +0200

    Outsource code for fluxcd version determination into own function
    This can become handy when automated tests are implemented and
    improves the overall structure of the code
    
    Signed-off-by: Jens Schneider <schneider@23technologies.cloud>

--- pkg/controller/lifecycle/actuator.go
+++ pkg/controller/lifecycle/actuator.go
@@ -77,28 +77,12 @@ func (a *actuator) Reconcile(ctx context.Context, ex *extensionsv1alpha1.Extensi
 		return err
 	}
 
-	// Create the resource for the flux installation
-	// If the ConfigMap in the garden cluster defines a version, use this version.
-	// Otherwise, simply use the latest version available on Github
-	var fluxVersion string
-	var exists bool
-	ghClient := github.NewClient(nil)
-	if fluxVersion, exists = fluxConfigMap.Data["fluxVersion"]; !exists {
-		a.logger.Info("You should specify a version for flux in the ConfigMap. I will simply use the latest release now.")
-		ghReleaseLatest, _, err := ghClient.Repositories.GetLatestRelease(ctx, "fluxcd", "flux2")
-		if err != nil {
-			return err
-		}
-		fluxVersion = *ghReleaseLatest.Name
-	} else
-	{
-		// Check if the release defined in the ConfigMap exists. If not, return an error
-		_, _, err := ghClient.Repositories.GetReleaseByTag(ctx, "fluxcd", "flux2", fluxVersion)
-		if err != nil {
-		a.logger.Error(err, "The flux version you specified does not exists. Please check the ConfigMap in the garden cluster.")
-			return err
-		}
+	fluxVersion, err := getFluxVersion(fluxConfigMap.Data)
+	if err != nil {
+		a.logger.Error(err, "I was not able to determine a flux release with respect to the version you defined. Check the configmap in the garden cluster for the version.")
+		return err
 	}
+	// Create the resource for the flux installation
 	shootResourceFluxInstall, err := createShootResourceFluxInstall(fluxVersion)
 	if err != nil {
 		return err
@@ -336,6 +320,30 @@ func getFluxKustomizationData() kustomizecontrollerv1beta2.Kustomization {
 	}
 
 	return ks
+}
+
+// getFluxVersion ...
+func getFluxVersion(fluxconfig map[string]string) (string, error) {
+
+	// If the ConfigMap in the garden cluster defines a version, use this version.
+	// Otherwise, simply use the latest version available on Github
+	var fluxVersion string
+	var exists bool
+	ghClient := github.NewClient(nil)
+	if fluxVersion, exists = fluxconfig["fluxVersion"]; !exists {
+		ghReleaseLatest, _, err := ghClient.Repositories.GetLatestRelease(context.Background(), "fluxcd", "flux2")
+		if err != nil {
+			return "", err
+		}
+		fluxVersion = *ghReleaseLatest.Name
+	} else {
+		// Check if the release defined in the ConfigMap exists. If not, return an error
+		_, _, err := ghClient.Repositories.GetReleaseByTag(context.Background(), "fluxcd", "flux2", fluxVersion)
+		if err != nil {
+			return "", err
+		}
+	}
+	return fluxVersion, nil
 }
 
 func getFluxInstallYaml(fluxVersion string) ([]byte, error) {
